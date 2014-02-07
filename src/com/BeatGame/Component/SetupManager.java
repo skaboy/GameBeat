e19bf87f66895cc06abc845c154a2ae948f91be0
Franck Arrecot <franck.arrecot@kdab.com>
2014-02-07 11:42:22 +0100

    Setup Of the BeatGame project

---
+++ src/com/BeatGame/Component/SetupManager.java
@@ -0,0 +1,14 @@
+package com.BeatGame.Component;
+
+import android.app.Activity;
+import android.os.Bundle;import com.BeatGame.Management.R;
+
+/**
+ * Created by Franck on 07/02/14.
+ */
+public class SetupManager extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_manager);
+    }
+}
