14e7fb5309b5258c77c921d08efad21902ccecdc
kemoudom <kemoudom@gmail.com>
2014-02-14 14:56:22 +0100

    Create circles

--- src/com/BeatGame/Manager/GameManager.java
+++ src/com/BeatGame/Manager/GameManager.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.BeatGame.Animation.MyAnimationView;
 import com.BeatGame.Management.R;
 
 public class GameManager extends Activity {
@@ -15,6 +19,17 @@ public class GameManager extends Activity {
 		setContentView(R.layout.game_manager);
 		Intent intent = getIntent();
 		System.out.println(intent.getStringExtra("level"));
+		
+		LinearLayout container = (LinearLayout) findViewById(R.id.container);
+        final MyAnimationView animView = new MyAnimationView(this);
+        container.addView(animView);
+
+        Button starter = (Button) findViewById(R.id.startButton);
+        starter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                animView.startAnimation();
+            }
+        });
 	}
 
 	@Override
